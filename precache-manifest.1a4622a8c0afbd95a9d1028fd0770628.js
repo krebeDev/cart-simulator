3fd870c8ddfaf41f740afaed05e587b1b8865481
Solomon Ekrebe <krebedev@gmail.com>
2020-01-17 13:50:09 +0100

    Updates

---
+++ precache-manifest.1a4622a8c0afbd95a9d1028fd0770628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eccdffc7db8ec15e00efde60389d15",
+    "url": "/cart-simulator/index.html"
+  },
+  {
+    "revision": "d30862038e1425ed4ebd",
+    "url": "/cart-simulator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9415a8b8b7f62aeeaf6b",
+    "url": "/cart-simulator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d30862038e1425ed4ebd",
+    "url": "/cart-simulator/static/js/2.2ad4c1d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cart-simulator/static/js/2.2ad4c1d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9415a8b8b7f62aeeaf6b",
+    "url": "/cart-simulator/static/js/main.2f0c74de.chunk.js"
+  },
+  {
+    "revision": "b4629dd1408298de3887",
+    "url": "/cart-simulator/static/js/runtime-main.7b15f506.js"
+  }
+]);
